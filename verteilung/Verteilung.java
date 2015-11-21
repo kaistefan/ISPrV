f102dcad8455d8f81e2bfb341021ea69f7f5ab7c
Antonia Berg <summsebien8@gmail.com>
2015-11-21 18:28:54 +0100

    einteilung erweitert

--- verteilung/Verteilung.java
+++ verteilung/Verteilung.java
@@ -14,32 +14,50 @@ public class Verteilung {
 		Student stud;
 		Step st;
 		Praktikum[] slots = new Praktikum[3];
-		stud = pras.getDreiF().get(1);
-		int i = 0;
-		
-		// Fach A
-		while (pras.getA().getPraktikas()[i].isFull()) {i++;}
-		slots[0]=pras.getA().getPraktikas()[i];
-		
-		// Fach B
-		while (pras.getB().getPraktikas()[i].isFull() || (slots[0].getId()==i)) {i++;}
-		slots[1]=pras.getB().getPraktikas()[i];
-		
-		// Fach C
-		while (pras.getC().getPraktikas()[i].isFull() || (slots[0].getId()==i) || (slots[1].getId()==i)) {i++;}
-		slots[2]=pras.getC().getPraktikas()[i];
-		
-		stud.setPraktikas(slots);
-		
-		// Step speichern
-		st = new Step(stud, pras.getDreiF());
-		steps.add(st);
 
+		/* wenn kein PLatz gefunden wird, dann...???
+		 * try catch versuchen, da array out of bound exception wenn kein platz gefunden wird
+		 */
+		while (!pras.getDreiF().isEmpty()) {
+			stud = pras.getDreiF().get(1);
+			int i = 0;
+
+			// Fach A
+			while (pras.getA().getPraktikas()[i].isFull()) {
+				i++;
+			}
+			slots[0] = pras.getA().getPraktikas()[i];
+
+			// Fach B
+			i=0;
+			while (pras.getB().getPraktikas()[i].isFull() || (slots[0].getId() == i)) {
+				i++;
+			}
+			slots[1] = pras.getB().getPraktikas()[i];
+
+			// Fach C
+			i=0;
+			while (pras.getC().getPraktikas()[i].isFull() || (slots[0].getId() == i) || (slots[1].getId() == i)) {
+				i++;
+			}
+			slots[2] = pras.getC().getPraktikas()[i];
+
+			stud.setPraktikas(slots);
+
+			// Step speichern
+			st = new Step(stud, pras.getDreiF());
+			steps.add(st);
+		}
 		// 2 Fächer
-		//woher krieg ich die Fächer die der student belegt
+		// woher krieg ich die Fächer die der Student belegt
+		while (!pras.getZweiF().isEmpty()) {
 
+		}
+		
 		// 1 Fach
+		while (!pras.getZweiF().isEmpty()) {
 
+		}
 		return true;
 	}
 }
