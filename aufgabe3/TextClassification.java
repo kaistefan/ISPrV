49abd31f7cce5debd4007cfb089af1aa616e6a52
kaistefankoester <kaistefankoester@web.de>
2016-01-11 09:18:21 +0100

    an der training methode weiter gearbeitet (wörter/Satzzeichen werden gezählt)

--- aufgabe3/TextClassification.java
+++ aufgabe3/TextClassification.java
@@ -10,13 +10,17 @@ public class TextClassification {
 	private Double totalWordsA,totalWordsB;
 	private Double totalCharsA,totalCharsB;
 	private Map<String,Double> wordsA,wordsB;
-	private Double numberCountA,numberCountB;
+	private Map<Character,Double> charsA,charsB;
+	private int numberCountA,numberCountB;
+	
 	
 	TextClassification(){
 		totalTextA=0.0;
 		totalSentencesA=0.0;
 		totalWordsA=0.0;
+		charsA=new HashMap<Character,Double>();
 		wordsA=new HashMap<String,Double>();
+		numberCountA=0;
 	}
 	
 	
@@ -24,23 +28,33 @@ public class TextClassification {
 		
 		for(String text : list){
 			totalTextA++;
-			String [] sentences=text.split(".");
+			String [] sentences=text.split("[.?!]");
 			totalSentencesA+=sentences.length;
 			for(String sentenc :sentences){
-				String [] words=sentenc.split(" ");
+				String [] words=sentenc.split("[ \t ]");
 				totalWordsA+=words.length;
 				for(String word :words){
-					if(word.matches("[0-9]")){
-						
+					String temp =word.replaceAll("[^A-Za-z0-9]", "");
+					if(!temp.equals("")){
+						if(temp.matches("[0-9]+")){
+							numberCountA++;
+						}
+						else{
+							if(wordsA.get(temp)==null)wordsA.put(temp, 0.0);		
+							wordsA.put(temp, wordsA.get(temp)+1);
+						}		
 					}
-					else{
-						if(wordsA.get(word)==null)wordsA.put(word, 0.0);		
-						wordsA.put(word, wordsA.get(word)+1);
-					}		
 				}
+			}
+			String satzZeichen=text.replaceAll("[A-Za-z0-9\u00C0-\u00FF  ]", "");
+			for(char c:satzZeichen.toCharArray()){
+				if(charsA.get(c)==null)charsA.put(c, 0.0);		
+				charsA.put(c, charsA.get(c)+1);
 			}
 			
 		}
-		
+		System.out.println(wordsA.size());
+		System.out.println(numberCountA);
+		System.out.println(charsA);
 	}
 }
--- aufgabe3/testAufgabe3.java
+++ aufgabe3/testAufgabe3.java
@@ -7,6 +7,8 @@ public class testAufgabe3 {
 	public static void main(String[] args) {
 	TxtFolderReader in = new TxtFolderReader ();
 	List<String> test =in.readFolder(args[1]);
+	TextClassification textcl =new TextClassification ();
 	System.out.println(test.size());
+	textcl.trainA(test);
 	}
 }
