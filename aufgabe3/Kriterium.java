bca0949497a4c189c87f9eaea921d02e2ebde9b2
Antonia Berg <summsebiene8@gmail.com>
2016-01-15 17:39:02 +0100

    Kriterium

--- aufgabe3/Kriterium.java
+++ aufgabe3/Kriterium.java
@@ -4,12 +4,32 @@ import java.util.HashMap;
 
 public class Kriterium {
 	Token kriterium;
-	HashMap resultsA;
-	HashMap resultsB;
+	HashMap<Integer,Integer> resultsA;
+	HashMap<Integer,Integer> resultsB;
 	
 	public Kriterium(Token t){
 		kriterium=t;
-		resultsA = new HashMap();
-		resultsB = new HashMap();
+		resultsA = new HashMap<Integer,Integer>();
+		resultsB = new HashMap<Integer,Integer>();
+	}
+
+	public HashMap<Integer, Integer> getResultsA() {
+		return resultsA;
+	}
+
+	public void setResultsA(HashMap<Integer, Integer> resultsA) {
+		this.resultsA = resultsA;
+	}
+
+	public HashMap<Integer, Integer> getResultsB() {
+		return resultsB;
+	}
+
+	public void setResultsB(HashMap<Integer, Integer> resultsB) {
+		this.resultsB = resultsB;
+	}
+
+	public Token getKriterium() {
+		return kriterium;
 	}
 }
